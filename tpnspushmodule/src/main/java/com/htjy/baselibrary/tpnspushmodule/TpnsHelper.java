08de3df23cbec302c2fe1cdc333c362272630747
linyibiao <295243617@qq.com>
2021-11-12 17:13:51 +0800

    [项目]baselibrary [模块]tpns [问题] [原因] [解决方案]tpns推送对接 [出现的版本] [解决的版本]

---
+++ tpnspushmodule/src/main/java/com/htjy/baselibrary/tpnspushmodule/TpnsHelper.java
@@ -0,0 +1,31 @@
+package com.htjy.baselibrary.tpnspushmodule;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushConfig;
+import com.tencent.android.tpush.XGPushManager;
+
+/**
+ * Created by besttimer on 2021/11/11
+ */
+public class TpnsHelper {
+
+    public static void init(Context context, boolean isDebug) {
+        XGPushConfig.enableDebug(context, isDebug);
+        XGPushManager.registerPush(context, new XGIOperateCallback() {
+            @Override
+            public void onSuccess(Object data, int flag) {
+                //token在设备卸载重装的时候有可能会变
+                Log.d("TPush", "注册成功，设备token为：" + data);
+            }
+
+            @Override
+            public void onFail(Object data, int errCode, String msg) {
+                Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
+            }
+        });
+    }
+
+}
